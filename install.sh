a260268cbfa6e446f4794b3ac96d89587087f92a
dabao1955 <79307765+dabao1955@users.noreply.github.com>
2023-06-03 20:50:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,9 @@ fi
 
 echo step 2/4 : install dotnet
 get_arch=`arch`
-if [[ $get_arch =~ "x86_64" ]];then
+if [ -d $HOME/dotnet*.tar.gz ];then
+    echo 
+else if [[ $get_arch =~ "x86_64" ]];then
     wget https://download.visualstudio.microsoft.com/download/pr/e89c4f00-5cbb-4810-897d-f5300165ee60/027ace0fdcfb834ae0a13469f0b1a4c8/dotnet-sdk-3.1.426-linux-x64.tar.gz
 elif [[ $get_arch =~ "aarch64" ]];then
     wget https://download.visualstudio.microsoft.com/download/pr/79f1cf3e-ccc7-4de4-9f4c-1a6e061cb867/68cab78b3f9a5a8ce2f275b983204376/dotnet-sdk-3.1.426-linux-arm64.tar.gz
@@ -21,15 +23,25 @@ elif [[ $get_arch =~ "armhf" ]];then
 else
     echo "unsupported architecture!!" ; exit 1
 fi
+fi
 rm -rf dotnet*.tar.gz
-mkdir -p $HOME/dotnet && tar zxf dotnet*.tar.gz -C $HOME/dotnet
+if [ -d $HOME/dotnet ];then
+echo 
+else
+    mkdir -p $HOME/dotnet && tar zxf dotnet*.tar.gz -C $HOME/dotnet
+fi
 
-echo step 3/4 : install e5renewx
-wget https://download.saika2077.repl.co/d/Guest/Microsoft365_E5_Renew_X.zip
-unzip Microsoft365_E5_Renew_X.zip -d $HOME/renewx
-rm -rf Microsoft365_E5_Renew_X.zip
+if [ -d $HOME/renewx ];then
+    echo
+else
+    echo step 3/4 : install e5renewx
+    wget https://download.saika2077.repl.co/d/Guest/Microsoft365_E5_Renew_X.zip
+    unzip Microsoft365_E5_Renew_X.zip -d $HOME/renewx
+    rm -rf Microsoft365_E5_Renew_X.zip
+fi
 
 echo step 4/4 : finish job
+
 if [ -f /usr/local/bin/starte5renewx ]
 then 
     echo
